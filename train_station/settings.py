99e1c45d27d985496309d819f05149b0fe8aaf27
Ihor Dmytriv <dmytriwihor@gmail.com>
2025-06-22 17:54:20 +0200

    Create user app and custom User model without email

--- train_station/settings.py
+++ train_station/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "railway.apps.StationConfig",
+    "user"
 ]
 
 MIDDLEWARE = [
@@ -125,3 +126,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "user.User"
