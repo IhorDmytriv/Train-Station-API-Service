0c437cda57a7d2697a36088b4a749dab48f14f37
Ihor Dmytriv <dmytriwihor@gmail.com>
2025-06-23 16:53:50 +0200

    Register models in admin panel

--- railway/admin.py
+++ railway/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
 
-# Register your models here.
+from railway.models import (
+    Journey,
+    Crew,
+    TrainType,
+    Train,
+    Station,
+    Route,
+    Order,
+    Ticket
+)
+
+
+models = [Journey, Crew, TrainType, Train, Station, Route, Order, Ticket]
+for model in models:
+    admin.site.register(model)
+
+admin.site.unregister(Group)
