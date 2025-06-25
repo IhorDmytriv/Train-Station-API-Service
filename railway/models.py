9ca2438d2b2905d3d4a9b6f7b27f007aa8d9607b
Ihor Dmytriv <dmytriwihor@gmail.com>
2025-06-25 18:52:25 +0200

    Add unique=True parameter to TrainType name and make migrations

--- railway/models.py
+++ railway/models.py
@@ -23,7 +23,7 @@ class Crew(models.Model):
 
 
 class TrainType(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, unique=True)
 
     def __str__(self):
         return self.name
