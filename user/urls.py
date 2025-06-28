be441c28dc59be343f9c1e10ff045e8ba741457c
Ihor Dmytriv <dmytriwihor@gmail.com>
2025-06-28 19:22:44 +0200

    Add update logic to User viewset

--- user/urls.py
+++ user/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 
-from user.views import CreateUserView
+from user.views import CreateUserView, ManageUserView
 
 urlpatterns = [
     path("create/", CreateUserView.as_view(), name="create"),
+    path("me/", ManageUserView.as_view(), name="manage"),
 ]
 
 app_name = "user"
--- user/views.py
+++ user/views.py
@@ -1,7 +1,13 @@
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import CreateAPIView, RetrieveUpdateAPIView
 
 from user.serializers import UserSerializer
 
 
 class CreateUserView(CreateAPIView):
     serializer_class = UserSerializer
+
+class ManageUserView(RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+
+    def get_object(self):
+        return self.request.user
