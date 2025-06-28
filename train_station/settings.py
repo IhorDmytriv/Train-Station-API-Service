e378b63b966ed8bd6b9e216d54cc9eaaaa940c58
Ihor Dmytriv <dmytriwihor@gmail.com>
2025-06-28 19:31:10 +0200

    Add Default Permission Classes

--- train_station/settings.py
+++ train_station/settings.py
@@ -140,9 +140,11 @@ INTERNAL_IPS = [
 
 REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
-    "PAGE_SIZE": 10
     "PAGE_SIZE": 10,
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
+    ]
 }
