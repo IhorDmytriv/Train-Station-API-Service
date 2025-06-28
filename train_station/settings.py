16a246b49bb42928bb90a427e6822f7271bd6fb4
Ihor Dmytriv <dmytriwihor@gmail.com>
2025-06-28 21:15:26 +0200

    Create custom permission and configure SIMPLE_JWT

--- train_station/settings.py
+++ train_station/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 import os
+from datetime import timedelta
 from pathlib import Path
 from dotenv import load_dotenv
 
@@ -145,6 +146,12 @@ REST_FRAMEWORK = {
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
     "DEFAULT_PERMISSION_CLASSES": [
-        "rest_framework.permissions.IsAuthenticated",
+        "railway.permissions.IsAdminOrIfAuthenticatedReadOnly",
     ]
+}
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(hours=1),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "ROTATE_REFRESH_TOKENS": False,
 }
